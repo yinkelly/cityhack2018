c386087aaaa9b8168bc38eddced7a9645fec050f
Yin Kelly <yin@sandtable.com>
2018-01-27 12:18:42 +0000

    Add GLA map api

--- app/client/map.js
+++ app/client/map.js
@@ -1,18 +1,32 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { HTTP } from 'meteor/http';
 
 import './map.html';
 
 Template.Map.onCreated(function mapOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  const token = '<GLA TOKEN>';
+
+  const URL = 'https://maps.london.gov.uk/gla/rest/services/apps/CityHack_aecom_Jan18_project_service01/MapServer/0/1';
+  HTTP.call('GET', URL, {
+    params:{"f":"json", "token": token}
+  }, (error, result) => {
+    if (!error) {
+      console.log(JSON.parse(result.content));
+    }
+  });
+
 });
 
 Template.Map.onRendered(function mapOnRendered() {
-  mapboxgl.accessToken = 'pk.eyJ1IjoieWlua2VsbHkiLCJhIjoiY2lzdTQzbGR1MDFheDJvczhsMnhiNnU3MCJ9.KFVBh4CjEN4LBaZ2cYc7sA';
+  mapboxgl.accessToken = '<MAPBOX TOKEN>';
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-0.140, 51.489],
+    zoom: 9.4
   });
 });
 
